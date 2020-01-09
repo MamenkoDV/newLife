c21c799994aa5b87e51585e8274f243affd89f8b
mamenkoDV <mamenko.dv@gmail.com>
2020-01-09 18:24:42 +0100

    2th task executed

---
+++ task1.2.php
@@ -0,0 +1,6 @@
+<?php
+//Задача. Напишите скрипт, который считает количество секунд в часе.
+
+$minute = 60; //has 60 second
+$hour = $minute * 60; // hour has 60 minutes
+echo "$hour seconds in one hour";
